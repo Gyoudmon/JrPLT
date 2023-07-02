044112c461170643aedeb45a5304f4c76a7fc12f
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-07-02 17:23:14 +0800

    remove `preface.scrbl`

--- literacy/literacy.rkt
+++ literacy/literacy.rkt
@@ -16,7 +16,7 @@
 ;;; Just in case for README.md
 (enter-digimon-zone!)
 
-;(tamer-indexed-block-hide-chapter-index #true)
+(tamer-indexed-block-hide-chapter-index #false)
 
 (tamer-block-label-separator #false)
 (tamer-block-label-tail "： ")
@@ -24,48 +24,6 @@
 
 (tamer-default-figure-label "图")
 (tamer-default-code-label "段")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define-for-syntax (parse-optional-pair stx)
-  (syntax-case stx []
-    [(min max) (list #'min #'max)]
-    [v #'v]))
-
-(define-syntax (discipline-desc stx)
-  (syntax-parse stx #:datum-literals []
-    [(_ (~alt (~once (~seq #:langs [langs ...]))
-              (~once (~seq #:grades [grades ...]))
-              (~once (~seq #:period period))
-              (~once (~seq #:note note)))
-        ...)
-     (with-syntax* ([(period:min period:max) (parse-optional-pair #'period)])
-       (syntax/loc stx
-         (nested #:style 'vertical-inset
-                 (para (emph "语言：")
-                       (add-between (list langs ...) " "))
-
-                 (para (emph "学段：")
-                       (add-between (list grades ...) " "))
-                 
-                 (para (emph "课时：")
-                       (add-between (map (λ [v] (elem #:style "disTag" ($ (number->string v))))
-                                         (filter exact-nonnegative-integer? (list period:min period:max)))
-                                    " - "))
-
-                 (para (emph "备注：") (emph note)))))]))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define tag:C++ (elem #:style "langTag" "C++"))
-(define tag:Python (elem #:style "langTag" "Python"))
-(define tag:Scratch (elem #:style "langTag" "Scratch"))
-
-(define tag:early (elem #:style "disTag" "小学低年级"))
-(define tag:elementary (elem #:style "disTag" "小学高年级"))
-(define tag:middle (elem #:style "disTag" "中学生"))
-(define tag:teacher (elem #:style "disTag" "教师"))
-
-(define tag:TLDR (elem #:style "tldrTag" "太长;不看"))
-(define tag:deep (elem #:style "deepTag" "少侠;留步"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define stone-image
