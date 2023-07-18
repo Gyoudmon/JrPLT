db4b7564c434d364da23a8f88366ccfda72760fe
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-07-18 23:38:53 +0800

    add pltmos

--- info.rkt
+++ info.rkt
@@ -11,15 +11,31 @@
 (define test-omit-paths 'all)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; WARNING
+; Package binaries and resources inside one repository might not be a good idea
+;   as the SDL2 header files causes "typedef redefinition with different types
+;   about the `size_t`" in macOS, in which we don't have to manage SDL2 header
+;   files manually.
+; Here we choose to link to the base shared object directly for generating
+;   additional shared objects, in which case the mentioned problem above
+;   is avoided.
+
 (define sdl2-config
   '((config SDL2)
     (include [windows "C:\\opt\\vcso\\include"])
     (libpath [windows "C:\\opt\\vcso\\lib"])))
 
+(define sdl2+config
+  (append sdl2-config
+          '((lib gydm.stem)
+            (include "digitama/big-bang-cpp/compiled/native")
+            (libpath "digitama/big-bang-cpp/compiled/native"))))
+
 (define native-compiled-subpath '())
 
 (define native-launcher-names
-  `(["digitama/big-bang-cpp/gydm.stem.cpp" so ,sdl2-config]))
+  `(["digitama/big-bang-cpp/gydm.stem.cpp" so ,sdl2-config]
+    ["digitama/pltmos/pltmos.cpp" so ,sdl2+config]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define typesettings
